1ccd8700821978e0ad2c5cb179c373523d6bc2cc
Lisa Stillwell <lisastillwell@bellsouth.net>
2022-01-11 20:18:30 -0500

    #1 add functions to handle main menu choices

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer")
-
-const employee_db = require('./lib/EmployeeDatabase');
+require('dotenv').config()
+const mysql = require("mysql2")
+const cTable = require('console.table');
 
 const options = [
     "View all Deparments", 
@@ -9,10 +10,20 @@ const options = [
     "Add Department",
     "Add Role",
     "Add Employee",
-    "Update Employee Role"
+    "Update Employee Role",
+    "Quit",
 ];
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB
+});
 
+doMainPrompt();
+
+function doMainPrompt() {
 inquirer.prompt([
     {
     type: "list",
@@ -22,5 +33,63 @@ inquirer.prompt([
     }
 ])
 .then(answers => {
+    switch(answers.choice) {
+        case options[0]:
+            viewAll("Department");
+            break;
 
+        case options[1]:
+            viewAll("Role");
+            break;
+        
+        case options[2]:
+            viewAll("Employee");
+            break;
+
+        case options[3]:
+            addDepartment();
+            break;
+
+        case options[4]:
+            addRole();
+            break;
+
+        case options[5]:
+            addEmployees();
+            break;
+
+        case options[6]:
+            updateEmployeeRole();
+            break;
+
+        case options[7]:
+            process.exit();
+    }
 });
+}
+
+function viewAll(tableName) {
+    const query = "SELECT * FROM " + tableName;
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log("\n" + tableName + "s");
+        console.table(res);
+        doMainPrompt();
+    });
+}
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function addEmployees() {
+
+}
+
+function updateEmployeeRole() {
+
+}
